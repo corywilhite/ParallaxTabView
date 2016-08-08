168ec8a9796ce102234978a11875065d1f2ec992
Cory Wilhite <cory.wilhite@gmail.com>
2016-08-08 15:43:47 -0400

    Stripping out remaining scroll view references

--- ParallaxTabView/ViewController.swift
+++ ParallaxTabView/ViewController.swift
@@ -8,42 +8,5 @@
 
 import UIKit
 
-class ViewController: UIViewController, ParallaxScrollViewDelegate {
-    
-    var castView: ParallaxScrollView!
-    
-    let headerView = UIView(frame: .zero)
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        headerView.backgroundColor = .greenColor()
-        let header = ParallaxHeaderController.with(
-            customView: headerView,
-            height: 150,
-            minimumHeight: 50
-        )
-        
-        castView = ParallaxScrollView(
-            frame: .zero,
-            parallaxHeader: header,
-            delegate: self
-        )
-        
-        view.addSubview(castView)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        castView.frame = view.frame
-        castView.contentSize = CGSize(width: view.bounds.width, height: 1000)
-    }
-    
-    func scrollViewDidScroll(scrollView: UIScrollView) {
-        
-    }
+class ViewController: UIViewController {
 }
