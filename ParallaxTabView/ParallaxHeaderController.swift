f086ced95f0b72c59e8dd5f582307d16df4d9c7d
Cory Wilhite <cory.wilhite@gmail.com>
2016-08-08 15:08:36 -0400

    refactoring for more idiomatic swift style and deleting the mode property as we only want to set the constraints of the content view to fill

--- ParallaxTabView/ParallaxHeaderController.swift
+++ ParallaxTabView/ParallaxHeaderController.swift
@@ -14,116 +14,74 @@ protocol ParallaxHeaderViewType {
 
 class ParallaxHeaderController: NSObject {
     
-    class func with(customView customView: UIView, height: CGFloat, minimumHeight: CGFloat, mode: Mode = .fill) -> ParallaxHeaderController {
+    class func with(customView customView: UIView, height: CGFloat, minimumHeight: CGFloat) -> ParallaxHeaderController {
         let header = ParallaxHeaderController()
         header.view = customView
         header.height = height
         header.minimumHeight = minimumHeight
-        header.mode = mode
         
         return header
     }
     
-    class func with(nibName nibName: String, height: CGFloat, minimumHeight: CGFloat, mode: Mode = .fill) -> ParallaxHeaderController {
+    class func with(nibName nibName: String, height: CGFloat, minimumHeight: CGFloat) -> ParallaxHeaderController {
         let header = ParallaxHeaderController()
         header.view = NSBundle.mainBundle().loadNibNamed(nibName, owner: self, options: nil).first as? UIView
         header.height = height
         header.minimumHeight = minimumHeight
-        header.mode = mode
         
         return header
     }
     
-    enum Mode {
-        case center
-        case fill
-        case top
-        case bottom
-    }
-    
-    weak var _scrollView: UIScrollView?
     weak var scrollView: UIScrollView? {
-        get {
-            return _scrollView
-        }
-        
-        set {
-            if _scrollView != newValue {
-                _scrollView = newValue
-                
-                guard let scrollView = _scrollView else { return }
-                
-                setScrollViewContentTopInset(scrollView.contentInset.top + height)
-                
-                scrollView.addSubview(contentView)
-                layoutContentView()
-                _isObserving = true
-            }
-        }
-    }
-    
-    
-    var _contentView: UIView?
-    var contentView: UIView {
-        get {
-            if _contentView == nil {
-                let newView = ParallaxView(frame: .zero)
-                newView.parent = self
-                newView.clipsToBounds = true
-                _contentView = newView
-            }
+        didSet {
+            guard let scrollView = scrollView else { return }
+            if scrollView == oldValue { return }
             
-            return _contentView!
+            set(contentInsetTop: scrollView.contentInset.top + height, of: scrollView)
+            
+            scrollView.addSubview(contentView)
+            layoutContentView()
         }
     }
     
-    var _view: UIView?
+    private(set) lazy var contentView: UIView = self.createContentView()
+    private func createContentView() -> UIView {
+        let view = ParallaxView(frame: .zero)
+        view.parent = self
+        view.clipsToBounds = true
+        return view
+    }
+    
     var view: UIView? {
-        get {
-            return _view
-        }
-        
-        set {
-            if newValue != _view {
-                _view = newValue
-                updateConstraints()
-            }
+        didSet {
+            if oldValue == view { return }
+            updateConstraints()
         }
     }
     
-    var _height: CGFloat = 0
-    var height: CGFloat {
-        get {
-            return _height
+    var height: CGFloat = 0 {
+        willSet {
+            if newValue == height { return }
+            let topInset = scrollView?.contentInset.top ?? 0
+            set(contentInsetTop: topInset - height + newValue, of: scrollView)
         }
-        set {
-            if _height != newValue {
-                
-                // even though when setting the scroll view, we nil check
-                // before setting the content inset top, we need the
-                // content layout to take place here along with
-                // the constraints
-                let topInset = scrollView?.contentInset.top ?? 0
-                
-                setScrollViewContentTopInset(topInset - _height + newValue)
-                
-                _height = newValue
-                updateConstraints()
-                layoutContentView()
-            }
+        didSet {
+            if oldValue == height { return }
+            updateConstraints()
+            layoutContentView()
         }
     }
     
-    func setScrollViewContentTopInset(top: CGFloat) {
+    private func set(contentInsetTop contentInsetTop: CGFloat, of scrollView: UIScrollView?) {
         guard let scrollView = scrollView else { return }
         
         var inset = scrollView.contentInset
         
         var offset = scrollView.contentOffset
-        offset.y += inset.top - top
+        offset.y += inset.top - contentInsetTop
         scrollView.contentOffset = offset
         
-        inset.top = top
+        inset.top = contentInsetTop
         scrollView.contentInset = inset
     }
     
@@ -137,16 +95,6 @@ class ParallaxHeaderController: NSObject {
         return x - 1
     }
     
-    var mode: Mode = .fill {
-        didSet {
-            if mode != oldValue {
-                updateConstraints()
-            }
-        }
-    }
-    
-    var _isObserving = false
-    
     func updateConstraints() -> Void {
         
         guard let view = view else { return debugPrint("nil view") }
@@ -155,25 +103,30 @@ class ParallaxHeaderController: NSObject {
         contentView.addSubview(view)
         view.translatesAutoresizingMaskIntoConstraints = false
         
-        switch mode {
-        case .fill:
-            setFillModeConstraints()
-        case .top: break
-        case .bottom: break
-        case .center: break
-        }
+        setFillConstraints(for: view, to: contentView)
     }
     
-    func setFillModeConstraints() -> Void {
-        guard let view = view else { return }
+    func setFillConstraints(for view: UIView, to contentView: UIView) {
         let binding = ["v": view]
-        contentView.addConstraints(
-            NSLayoutConstraint.constraintsWithVisualFormat("H:|[v]|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: binding)
-                + NSLayoutConstraint.constraintsWithVisualFormat("V:|[v]|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: binding)
+        
+        let layoutFormatOption = NSLayoutFormatOptions(rawValue: 0)
+        
+        let horizConstraints = NSLayoutConstraint.constraintsWithVisualFormat(
+            "H:|[v]|",
+            options: layoutFormatOption,
+            metrics: nil,
+            views: binding
         )
         
+        let vertConstraints = NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:|[v]|",
+            options: layoutFormatOption,
+            metrics: nil,
+            views: binding
+        )
+        
+        contentView.addConstraints(horizConstraints + vertConstraints)
     }
-    
     
     func layoutContentView() -> Void {
         guard let scrollView = scrollView else { return }
