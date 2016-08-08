abaa7da34659696dd6861fc197b22bb3efc52638
Cory Wilhite <cory.wilhite@gmail.com>
2016-08-08 15:52:03 -0400

    Removing Main storyboard, creating a ParallaxTabViewController file and setting the root view controller programatically

---
+++ ParallaxTabView/ParallaxTabViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ParallaxTabViewController.swift
+//  ParallaxTabView
+//
+//  Created by Cory Wilhite on 8/8/16.
+//  Copyright © 2016 Cory Wilhite. All rights reserved.
+//
+
+import UIKit
+
+class ParallaxTabViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .whiteColor()
+    }
+}
